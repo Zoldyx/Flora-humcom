1f5ba1b4b9d71f32a3904dcc0c10a81febdb0370
Enrico Flora <enrico20flora@gmail.com>
2021-05-15 21:34:56 +0800

    ARERARE

--- site/FINALS/STYLES/ASSIGN14A.js
+++
@@ -1,4 +0,0 @@
-document.write("name:Enrico Charles Flora\ncourse:BSIT\nyear:1st year\nsubjectcode:HUMCOM\nsection:IAA1\nsubjectdescription:HUMAN COMPUTERS");
-
-
-alert("name:Enrico Charles Flora\ncourse:BSIT\nyear:1st year\nsubjectcode:HUMCOM\nsection:IAA1\nsubjectdescription:HUMAN COMPUTERS");
---
+++ site/FINALS/STYLES/ASSIGN14D.js
@@ -0,0 +1,4 @@
+document.write("name:Enrico Charles Flora\ncourse:BSIT\nyear:1st year\nsubjectcode:HUMCOM\nsection:IAA1\nsubjectdescription:HUMAN COMPUTERS");
+
+
+alert("name:Enrico Charles Flora\ncourse:BSIT\nyear:1st year\nsubjectcode:HUMCOM\nsection:IAA1\nsubjectdescription:HUMAN COMPUTERS");
