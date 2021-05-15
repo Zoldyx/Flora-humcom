fb68ec1c72c07c41a1184671c53a4bfd47bbfdad
Enrico Flora <enrico20flora@gmail.com>
2021-05-15 21:50:40 +0800

    ADADWDA

---
+++ site/FINALS/STYLES/ASSIGN14A.js
@@ -0,0 +1,5 @@
+document.getElementById("clean").innerHTML = "Enrico Charles Flora";
+document.getElementById("clean1").innerHTML = "BSIT";
+document.getElementById("clean2").innerHTML = "1st year";
+document.getElementById("clean3").innerHTML = "HUMCOM";
+document.getElementById("clean4").innerHTML = "HUMAN COMPUTERS";
