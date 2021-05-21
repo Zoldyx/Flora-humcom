b7db36bcde8972790a57822689f8f809f6147ba2
Enrico Flora <enrico20flora@gmail.com>
2021-05-21 19:37:40 +0800

    421521516

--- site/FINALS/STYLES/FINALS.js
+++ site/FINALS/STYLES/FINALS.js
@@ -1,27 +1,4 @@
-(function(document) {
-    var metas = document.getElementsByTagName('meta'),
-        changeViewportContent = function(content) {
-            for (var i = 0; i < metas.length; i++) {
-                if (metas[i].name == "viewport") {
-                    metas[i].content = content;
-                }
-            }
-        },
-        initialize = function() {
-            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
-        },
-        gestureStart = function() {
-            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
-        },
-        gestureEnd = function() {
-            initialize();
-        };
+document.write("name:Enrico Charles Flora\ncourse:BSIT\nyear:1st year\nsubjectcode:HUMCOM\nsection:IAA1\nsubjectdescription:HUMAN COMPUTERS");
 
 
-    if (navigator.userAgent.match(/iPhone/i)) {
-        initialize();
-
-        document.addEventListener("touchstart", gestureStart, false);
-        document.addEventListener("touchend", gestureEnd, false);
-    }
-})(document);
+alert("name:Enrico Charles Flora\ncourse:BSIT\nyear:1st year\nsubjectcode:HUMCOM\nsection:IAA1\nsubjectdescription:HUMAN COMPUTERS");
